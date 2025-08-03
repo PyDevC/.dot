8241bf7433397fa580f4f725b6b1adfdff3d5771
PyDevC <anuskcse@gmail.com>
2025-08-03 06:14:50 -0400

    [nvim]: qmlls lsp neovim

--- config/nvim/lua/pydevc/plugins/lazyloading/lsp.lua
+++ config/nvim/lua/pydevc/plugins/lazyloading/lsp.lua
@@ -1,108 +1,108 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            "folke/neodev.nvim",
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-            "WhoIsSethDaniel/mason-tool-installer.nvim",
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "folke/neodev.nvim",
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
 
-            { "j-hui/fidget.nvim", opts = {} },
+      { "j-hui/fidget.nvim", opts = {} },
 
-            -- Schema information
-            "b0o/SchemaStore.nvim",
-        },
-        config = function()
-            require("neodev").setup {
-            }
-
-            local capabilities = nil
-            if pcall(require, "cmp_nvim_lsp") then
-                capabilities = require("cmp_nvim_lsp").default_capabilities()
-            end
-
-            local lspconfig = require "lspconfig"
-
-            local servers = {
-                lua_ls = true,
-                pyright = true,
-
-                jsonls = {
-                    settings = {
-                        json = {
-                            schemas = require("schemastore").json.schemas(),
-                            validate = { enable = true },
-                        },
-                    },
-                },
-
-                yamlls = {
-                    settings = {
-                        yaml = {
-                            schemaStore = {
-                                enable = false,
-                                url = "",
-                            },
-                            schemas = require("schemastore").yaml.schemas(),
-                        },
-                    },
-                },
-
-                clangd = {
-                    -- TODO: Could include cmd, but not sure those were all relevant flags.
-                    --    looks like something i would have added while i was floundering
-                    init_options = { clangdFileStatus = true },
-                    filetypes = { "c", "cpp" },
-                },
-            }
-
-            local servers_to_install = vim.tbl_filter(function(key)
-                local t = servers[key]
-                if type(t) == "table" then
-                    return not t.manual_install
-                else
-                    return t
-                end
-            end, vim.tbl_keys(servers))
-
-            for name, config in pairs(servers) do
-                if config == true then
-                    config = {}
-                end
-                config = vim.tbl_deep_extend("force", {}, {
-                    capabilities = capabilities,
-                }, config)
-
-                lspconfig[name].setup(config)
-            end
-
-            local disable_semantic_tokens = {
-                lua = true,
-            }
-
-            vim.diagnostic.config({
-                virtual_text = true,
-                signs = true,
-                update_in_insert = false,
-                underline = true,
-                severity_sort = false,
-                float = true,
-            })
-
-            vim.api.nvim_create_autocmd("LspAttach", {
-                callback = function(e)
-                    local opts = { buffer = e.buf }
-                    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-                    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-                    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-                    vim.keymap.set("n", "<leader>dt", function() vim.diagnostic.open_float() end, opts) -- extremly useful in diagnostics
-                    vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-                    vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
-                    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-                    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-                    vim.keymap.set({ "n", "x" }, "<leader>f", vim.lsp.buf.format, { buffer = 0 })
-                end
-            })
-        end,
+      -- Schema information
+      "b0o/SchemaStore.nvim",
     },
+    config = function()
+      require("neodev").setup {
+      }
+
+      local capabilities = nil
+      if pcall(require, "cmp_nvim_lsp") then
+        capabilities = require("cmp_nvim_lsp").default_capabilities()
+      end
+
+      local lspconfig = require "lspconfig"
+      local servers = {
+        qmlls = true,
+        lua_ls = true,
+        pyright = true,
+
+        jsonls = {
+          settings = {
+            json = {
+              schemas = require("schemastore").json.schemas(),
+              validate = { enable = true },
+            },
+          },
+        },
+
+        yamlls = {
+          settings = {
+            yaml = {
+              schemaStore = {
+                enable = false,
+                url = "",
+              },
+              schemas = require("schemastore").yaml.schemas(),
+            },
+          },
+        },
+
+        clangd = {
+          -- TODO: Could include cmd, but not sure those were all relevant flags.
+          --    looks like something i would have added while i was floundering
+          init_options = { clangdFileStatus = true },
+          filetypes = { "c", "cpp" },
+        },
+      }
+
+      local servers_to_install = vim.tbl_filter(function(key)
+        local t = servers[key]
+        if type(t) == "table" then
+          return not t.manual_install
+        else
+          return t
+        end
+      end, vim.tbl_keys(servers))
+
+      for name, config in pairs(servers) do
+        if config == true then
+          config = {}
+        end
+        config = vim.tbl_deep_extend("force", {}, {
+          capabilities = capabilities,
+        }, config)
+
+        lspconfig[name].setup(config)
+      end
+
+      local disable_semantic_tokens = {
+        lua = true,
+      }
+
+      vim.diagnostic.config({
+        virtual_text = true,
+        signs = true,
+        update_in_insert = false,
+        underline = true,
+        severity_sort = false,
+        float = true,
+      })
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(e)
+          local opts = { buffer = e.buf }
+          vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+          vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+          vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+          vim.keymap.set("n", "<leader>dt", function() vim.diagnostic.open_float() end, opts) -- extremly useful in diagnostics
+          vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+          vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+          vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+          vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+          vim.keymap.set({ "n", "x" }, "<leader>f", vim.lsp.buf.format, { buffer = 0 })
+        end
+      })
+    end,
+  },
 }
