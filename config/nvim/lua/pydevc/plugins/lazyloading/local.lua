aba8c4057b1b8e3c93ca5c777cf3849aa1d16954
PyDevC <anuskcse@gmail.com>
2025-08-23 06:53:26 -0400

    update<noting special>

--- config/nvim/lua/pydevc/plugins/lazyloading/local.lua
+++ config/nvim/lua/pydevc/plugins/lazyloading/local.lua
@@ -1,6 +1,11 @@
 return {
-  dir = "~/personal/project/model-cmp.nvim",
-  config = function()
-    require('model_cmp').setup({})
-  end,
+  --dir = "~/personal/project/model-cmp.nvim",
+  --config = function()
+  --  require("model_cmp").setup({
+  --    delay = 1,
+  --    virtualtext = {
+  --      enable = true
+  --    }
+  --  })
+  --end,
 }
