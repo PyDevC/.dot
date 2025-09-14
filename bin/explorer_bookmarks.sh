4fb393c20ce6e41978ed7705ff8b458c1e4babbd
PyDevC <anuskcse@gmail.com>
2025-09-14 01:39:35 -0400

    Created custom bookmarks for explorer

---
+++ bin/explorer_bookmarks.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+b_dir=("Documents" "Downloads" "Pictures" "Videos" "personal")
+bookmark_path="$HOME/.config/gtk-3.0/bookmarks"
+
+echo "cleaning bookmarks..."
+echo "" > $bookmark_path
+
+for dir in ${b_dir[@]}; do
+    bookmark="file:///home/$USER/$dir"
+    echo "$bookmark"
+    echo $bookmark >> $bookmark_path
+done
+
+echo "created bookmarks"
